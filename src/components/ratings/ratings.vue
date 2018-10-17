4451c12ce182d50eaece21347ec919b919e7975d
FelixXG <felixxg@foxmail.com>
2018-10-17 21:23:15 +0800

    详情评论以及ratings

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -8,6 +8,20 @@
           <div class="rank">高于周边商家{{seller.rankRate}}%</div>
         </div>
         <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
         </div>
       </div>
     </div>
@@ -15,11 +29,21 @@
 </template>
 
 <script>
+    import BSCroll from 'better-scroll';
+    import {fromatDate} from '../../common/js/date';
+    import star from '../star/star.vue';
+    import ratingselect from '../ratingselect/ratingselect.vue';
+    import split from '../split/split.vue'
     export default {
       props: {
         seller: {
           type: Object
         }
+      },
+      components: {
+        star,
+        split,
+        ratingselect
       }
     };
 </script>
@@ -42,15 +66,58 @@
         width: 137px
         border-right: 1px solid rgba(7,17,27,0.1)
         text-align:center
-        background-color #f01414
+        @media only screen and (max-width: 320px)
+          flex: 0 0 120px
+          width: 120px
         .score
           margin-bottom: 6px
           line-height: 28px
           font-size: 24px
           color: rgb(255,153,0)
+        .title
+          margin-bottom: 8px
+          line-height: 12px
+          font-size: 12px
+          color: rgb(7, 17, 27)
+        .rank
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
       .overview-right
         flex: 1
         padding: 6px 0 6px 24px
+        @media only screen and (max-width: 320px)
+          padding-left: 4px
+        .score-wrapper
+          margin-bottom: 8px
+          font-size: 0
+          .title
+            display: inline-block
+            line-height: 18px
+            vertical-align: top
+            font-size: 12px
+            color: rgb(7,17,27)
+          .star
+            display: inline-block
+            margin: 0 12px
+            vertical-align: top
+          .score
+            display: inline-block
+            line-height: 18px
+            vertical-align: top
+            font-size: 12px
+            color: rgb(255, 153, 0)
+        .delivery-wrapper
+          font-size: 0
+          .title
+            line-height: 18px
+            font-size: 12px
+            color: rgb(7,17,27)
+          .delivery
+            margin-left: 12px
+            font-size: 12px
+            color: rgb(147,153,159)
+
 
 
 </style>
